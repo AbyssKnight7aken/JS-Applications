d278af70dfc8b47704a78e73a387f589abb25d73
AbyssKnight7aken <7akenbg@gmail.com>
2023-05-22 23:39:45 +0300

    layout.js

---
+++ 24.Regular Exam/Fruitipedia/src/views/layout.js
@@ -0,0 +1,37 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+
+//TODO Replace with actual layout
+//layout shows content and shows if user is logged 
+export const layoutTemplate = (userData, content) => html`
+<header>
+<!-- Navigation -->
+<a id="logo" href="/"> <img id="logo-img" src="/images/logo.png" alt=""/></a>
+
+<nav>
+    <div>
+    <a href="/dashboard">Fruits</a>
+    <a href="/search">Search</a>
+    </div>
+
+    <!-- Logged-in users -->
+    ${userData
+        ? html`<div class="user">
+                    <a href="/create">Add Fruit</a>
+                    <a href="/logout">Logout</a>
+                </div>`
+        : html`<div class="guest">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+    </div>`}
+    </div>
+</nav>
+</header>
+
+<main id="main-content">${content}</main>
+
+
+<footer>
+<p>@Fruitipedia</p>
+</footer>
+    `;
