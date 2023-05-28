c6de655d1d82d20144894a4ae1d0b0899f99962e
AbyssKnight7aken <7akenbg@gmail.com>
2023-05-28 21:58:20 +0300

    create.js

---
+++ 24.Regular Exam/games-play/src/views/create.js
@@ -0,0 +1,44 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { createSubmitHandler } from "../util.js";
+import { createItem } from "../data/services.js";
+
+const createTemplate = (onSubmit) => html`
+<section id="create-page" class="auth">
+            <form @submit=${onSubmit} id="create">
+                <div class="container">
+
+                    <h1>Create Game</h1>
+                    <label for="leg-title">Legendary title:</label>
+                    <input type="text" id="title" name="title" placeholder="Enter game title...">
+
+                    <label for="category">Category:</label>
+                    <input type="text" id="category" name="category" placeholder="Enter game category...">
+
+                    <label for="levels">MaxLevel:</label>
+                    <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1">
+
+                    <label for="game-img">Image:</label>
+                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo...">
+
+                    <label for="summary">Summary:</label>
+                    <textarea name="summary" id="summary"></textarea>
+                    <input class="btn submit" type="submit" value="Create Game">
+                </div>
+            </form>
+        </section>
+      `;
+
+export async function createPage(ctx) {
+  
+
+  async function onSubmit(data) {
+
+    if (Object.values(data).some(x => x === '')) {
+      return alert('All fields are required!');
+    }
+
+    await createItem(data);
+    ctx.page.redirect('/');
+  }
+  ctx.render(createTemplate(createSubmitHandler(onSubmit)));
+}
