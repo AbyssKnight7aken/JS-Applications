e93c6b0e35770da02e531d7476240434c6620db1
AbyssKnight7aken <7akenbg@gmail.com>
2023-11-01 22:46:10 +0200

    create.js

---
+++ 24.Regular Exam/sole-mates/src/views/create.js
@@ -0,0 +1,105 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { createSubmitHandler } from "../util.js";
+import { createItem } from "../data/services.js";
+
+const createTemplate = (onSubmit) => html`
+<section id="create">
+          <div class="form">
+            <h2>Add item</h2>
+            <form @submit=${onSubmit} class="create-form">
+              <input
+                type="text"
+                name="brand"
+                id="shoe-brand"
+                placeholder="Brand"
+              />
+              <input
+                type="text"
+                name="model"
+                id="shoe-model"
+                placeholder="Model"
+              />
+              <input
+                type="text"
+                name="imageUrl"
+                id="shoe-img"
+                placeholder="Image url"
+              />
+              <input
+                type="text"
+                name="release"
+                id="shoe-release"
+                placeholder="Release date"
+              />
+              <input
+                type="text"
+                name="designer"
+                id="shoe-designer"
+                placeholder="Designer"
+              />
+              <input
+                type="text"
+                name="value"
+                id="shoe-value"
+                placeholder="Value"
+              />
+
+              <button type="submit">post</button>
+            </form>
+          </div>
+        </section>
+    `;
+
+export function createPage(ctx) {
+    ctx.render(createTemplate(createSubmitHandler(onSubmit)));
+
+    async function onSubmit(data){
+
+        if(Object.values(data).some(x => x === '')) {
+            return alert ('All fields are required!')
+        }
+        
+        await createItem(data);
+        ctx.page.redirect('/dashboard');
+    }
+}
+
+
+{/* <section id="create-page" class="create">
+    <form @submit=${onSubmit} id="create-form" action="" method="">
+        <fieldset>
+            <legend>Add new Book</legend>
+            <p class="field">
+                <label for="title">Title</label>
+                <span class="input">
+                    <input type="text" name="title" id="title" placeholder="Title">
+                </span>
+            </p>
+            <p class="field">
+                <label for="description">Description</label>
+                <span class="input">
+                    <textarea name="description" id="description" placeholder="Description"></textarea>
+                </span>
+            </p>
+            <p class="field">
+                <label for="image">Image</label>
+                <span class="input">
+                    <input type="text" name="imageUrl" id="image" placeholder="Image">
+                </span>
+            </p>
+            <p class="field">
+                <label for="type">Type</label>
+                <span class="input">
+                    <select id="type" name="type">
+                        <option value="Fiction">Fiction</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Mistery">Mistery</option>
+                        <option value="Classic">Clasic</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Add Book">
+        </fieldset>
+    </form>
+</section> */}
